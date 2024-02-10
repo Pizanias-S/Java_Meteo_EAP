d41096eedb9399d42512b51cc7246bf10ac7c120
Pizanias-S <perolocooo@hotmail.com>
2024-02-10 20:31:43 +0200

    Test commit

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.mavenproject1;
 
-/**
- *
- * @author perol
- */
 public class Mavenproject1 {
 
     public static void main(String[] args) {
