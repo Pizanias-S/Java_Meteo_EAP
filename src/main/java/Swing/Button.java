3776c98ecda0fcea03b89aef897c94a62421f968
Pizanias-S <perolocooo@hotmail.com>
2024-03-10 00:15:10 +0200

    Small fixes

--- src/main/java/Swing/Button.java
+++ src/main/java/Swing/Button.java
@@ -4,7 +4,6 @@ import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
 
